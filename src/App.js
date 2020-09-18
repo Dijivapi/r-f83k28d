ed5963b842f11e7b7216eeea65705d3653e60959
Diana <dianitavan@gmail.com>
2020-09-17 19:29:51 -0500

    daña los cambios al guardar

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
-
 class App extends Component {
     constructor() {
         super();
         this.increment = this.increment.bind(this);
 
-        this.state = {
-            value: 0
-        }
+        this.state = { value: 0 }
     }
 
     render() {
         return ( <
             div >
             <
-            span className = "value" > { this.state.value } < /span>  <
+            span className = "value" > { this.state.value } < /span> <
             button id = "inc"
-            onClick = { this.increment } > Incrementa < /button>  <
+            onClick = { this.increment } > Incrementa < /button> <
             /div>
         );
     }
 
     increment() {
-        var currentValue = this.state.value
-        this.setState({
-            value: currentValue + 1
-        });
+        this.setState({ value: this.state.value + 1 });
     }
 }
-
-export default App;
